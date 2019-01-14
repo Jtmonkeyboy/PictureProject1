12b561de7b74a69b356ed9d14c98830987931a59
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2019-01-14 11:34:11 -0700

    shiftUpDown

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -254,6 +254,32 @@ public class Picture extends SimplePicture
 	  }
   }
   
+  public void shiftUpDown(int amount)
+  {
+	  Pixel[][] pixels = this.getPixels2D();
+	  Picture temp = new Picture(this);
+	  Pixel[][] copied = temp.getPixels2D();
+	  
+	  int shiftedValue = amount;
+	  int width = pixels[0].length;
+	  
+	  for(int row = 0; row < pixels.length; row++)
+	  {
+		  for(int col = 0; col < pixels[0].length; col++)
+		  {
+			  shiftedValue = (row + amount) % width;
+			  copied[row][col].setColor(pixels[shiftedValue][col].getColor());
+		  }
+	  }
+	  for(int row = 0; row < pixels.length; row++)
+	  {
+		  for(int col = 0; col < pixels[0].length; col++)
+		  {
+			  pixels[row][col].setColor(copied[row][col].getColor());
+		  }
+	  }
+  }
+  
   public Color randomColor()
   {
 	  
